6f64040f34aadd12eb9d1d0c18fe77a04bdb18a7
Fibonatcchi <fabian.liebermann@gmail.com>
2026-09-24 15:05:28 +0200

    Add files via upload

--- Fisch.js
+++ Fisch.js
@@ -807,19 +807,13 @@ resetButton.addEventListener(
 
 async function startApp() {
 
-    alert("1 - Start");
-
     await challengeLaden();
-
-    alert("2 - Challenge geladen");
 
     await teilnehmerAusSupabaseLaden();
 
-    alert("3 - Teilnehmer geladen");
-
     await fischartenAusSupabaseLaden();
 
-    alert("4 - Fischarten geladen");
+    anzeigen();
 }
 
 startApp();
